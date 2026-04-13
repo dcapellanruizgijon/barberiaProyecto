1cf7b5afaf1c4f0664ee60e139b3fbf0b41ae66d
DYerpesruizgijon <david.yerpes-gordillo@iesruizgijon.com>
2026-04-13 09:21:45 +0200

    Añadimos la tabla de reseñas

---
+++ back/src/main/java/com/example/back/Resenas.java
@@ -0,0 +1,13 @@
+package com.example.back;
+
+public class Resenas {
+
+    private Integer id;
+    private Integer id_usuario;
+    private Integer id_barberia;
+    private long comentario;
+    private Integer puntuacion;
+    
+    //Si nos columpiamos mucho
+    // private long imgUrl;
+}
