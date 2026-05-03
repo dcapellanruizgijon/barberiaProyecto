ff3a8f3b3efa68fba12a99f6370dc488f35f9b27
DYerpesruizgijon <david.yerpes-gordillo@iesruizgijon.com>
2026-05-03 16:08:04 +0200

    CAMBIOS EN EL SERVICIO PA QUE VAYA BIEN DIOS

--- back/src/main/java/com/example/back/Servicio.java
+++ back/src/main/java/com/example/back/Servicio.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,7 +21,7 @@ public class Servicio {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
-    private double precio;
+    private Double precio;
     private Integer duracion; // Duración en minutos
 
     @ManyToOne
