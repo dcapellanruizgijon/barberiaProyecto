e58f63c92e2895e7a9cf2f15620ba33488c9621a
DYerpesruizgijon <david.yerpes-gordillo@iesruizgijon.com>
2026-05-06 09:34:15 +0200

    Citas ya lista coño

--- back/src/main/java/com/example/back/repositorios/CitaRepositorio.java
+++ back/src/main/java/com/example/back/repositorios/CitaRepositorio.java
@@ -3,12 +3,13 @@ package com.example.back.repositorios;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.back.Cita;
 
-
 public interface CitaRepositorio extends JpaRepository<Cita, Long> {
-    
+
     // Busca las citas de una barbería en un día específico
     // fechaHora formateada como "YYYY-MM-DD"
     List<Cita> findByBarberiaIdAndFechaHoraStartingWith(Long barberiaId, String fecha);
@@ -17,5 +18,7 @@ public interface CitaRepositorio extends JpaRepository<Cita, Long> {
     List<Cita> findByBarberoId(Long barberoId);
 
     List<Cita> findByClienteId(Long clienteId);
-    List<Cita> findByBarberiaId(Long barberiaId);
+
+@Query("SELECT c FROM Cita c JOIN FETCH c.servicioId JOIN FETCH c.cliente WHERE c.barberia.id = :barberiaId")
+List<Cita> findByBarberiaId(@Param("barberiaId") Long barberiaId);
 }
