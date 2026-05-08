3c74075156fc58a3dc9b6a817c4f3b356a65dc86
DYerpesruizgijon <david.yerpes-gordillo@iesruizgijon.com>
2026-05-08 14:48:40 +0200

    Mas cambios para que funcione las reseñas

--- back/src/main/java/com/example/back/Resenas.java
+++ back/src/main/java/com/example/back/Resenas.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,13 +21,18 @@ public class Resenas {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    // Cambiamos Long por la Entidad Cliente
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", insertable = false, updatable = false)
+    private Cliente usuario;
+
     @Column(name = "id_usuario")
     private Long idUsuario;
+
     @Column(name = "id_barberia")
     private Long idBarberia;
+    
     private String comentario;
     private Integer puntuacion;
-    
-    //Si nos columpiamos mucho
-    // private long imgUrl;
 }
